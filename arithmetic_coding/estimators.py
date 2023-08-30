c14c132c28a4af31e12b0c13a030fdb03d724068
Hồng Hải Phan Vũ <honghaix5nh@gmail.com>
2023-08-29 20:55:51 -0400

    add docs and refactor get_symbol

--- arithmetic_coding/estimators.py
+++ arithmetic_coding/estimators.py
@@ -1,8 +1,10 @@
 import numpy as np
+import bisect
 
 from abc import ABC, abstractmethod
 from collections import Counter
 
+from . import utils
 
 class Estimator(ABC):
 
@@ -31,76 +33,80 @@ class FrequencyEstimator(Estimator):
   A simple static estimator using frequency table.
   """
 
-  def __init__(self, data, stream_type):
+  def __init__(self, stream_type):
     self.stream_type = stream_type
+    
+    
+  def fit(self, data):
+    """
+    Learn the data's frequency.
+    """
     stream, length = self.get_stream(data)
     self.counter = Counter(stream)
-    total = sum(self.counter.values())
-
+    self.total = sum(self.counter.values())
     self.symbols = sorted(self.counter.keys())
     self.index = {s:idx for idx, s in enumerate(self.symbols)}
     self.probability = np.array([self.counter[s] for s in self.symbols])
-    self.probability = self.probability/total
+    self.probability = self.probability/self.total
     self.cdf = np.array([self.probability[:idx].sum() for idx in range(len(self.symbols))])
     self.cdf = np.append(self.cdf, [1.0])
-    
+
 
   def get_context(self, stream):
     """
-    This estimator doesn't need context.
+    This estimator is a fixed context.
     """
+
     pass
 
   def get_stream(self, input):
-    """
-    Turn inputs into stream of symbols.
 
-    Args:
-      inp: a string
-
-    Returns:
-      List of symbols, i.e. bytes, characters, or words.
-    """
-
-    # Character model
-    if self.stream_type == 'char':
-      return list(input), len(input)
-    
-    # Word model
-    elif self.stream_type == 'word':
-      return input.split(' '), len(input.split(' '))
+    return utils.get_stream_text(input, mode=self.stream_type)
 
   def get_upper(self, symbol, context):
+    """
+    Get the CDF of the current symbol's index + 1.
+    If it's the last index, rounding up to 1.
+
+    Args:
+      symbol: A symbol from the stream of data.
+      context: Not used.
+
+    Return:
+      Probability in the [0, 1] range.
+    """
+
     if self.index[symbol] == len(self.symbols)-1:
       return 1
     return self.cdf[self.index[symbol] + 1]
 
   def get_lower(self, symbol, context):
+    """
+    Get the CDF of the current symbol's index.
+
+    Args:
+      symbol: A symbol from the stream of data.
+      context: Not used.
+
+    Return:
+      Probability in the [0, 1) range.
+    """
+
     return self.cdf[self.index[symbol]]
 
-  def get_prob(self, symbol, context):
-    return self.probability[self.index[symbol]]
-
-  def get_symbol(self, prob, context):
+  def get_symbol(self, probability, context):
     """
-    Find the symbol from given probability range
-    """
-    assert prob <= 1, 'probability must <= 1'
-    first = 0
-    last = len(self.symbols)
+    Find the corresponding symbol from given probability range using binary
+    search.
 
-    while last - first > 1:
-      middle = (last+first) // 2
-      # print(prob, middle, self.cdf[middle])
-      if prob < self.cdf[middle]:
-        last = middle
-      else:
-        first = middle
-    assert first+1 == last
-    
-    # Possible problem with float precision
-    if not self.cdf[first] <= prob <= self.cdf[first+1]:
-      raise ValueError('Probability not in range.')
-      
-    # print('Result: {}'.format(self.symbols[first]))
-    return self.symbols[first]
+    Args:
+      probability: The representative value of current probability range [a, b]
+      where a <= probability < b.
+      context: Not used.
+
+    Return:
+      The corresponding symbol to the probability range.
+    """
+
+    symbol_idx = bisect.bisect(self.cdf, probability)-1
+    return self.symbols[symbol_idx]
